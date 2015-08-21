6a7f66f7a5282bfef175100f543819a9f08a6e55
tungpt247 <tungpt247@gmail.com>
2015-08-21 09:53:31 +0700

    Add extend helper

---
+++ Recipes/Helpers.swift
@@ -0,0 +1,13 @@
+//
+//  Helpers.swift
+//  Recipes
+//
+//  Created by tungphan on 8/20/15.
+//  Copyright (c) 2015 tp. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+typealias RecipeArray = [Results <Recipe>]
+typealias ResultRecipes = Results <Recipe>
